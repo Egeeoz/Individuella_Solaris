cc79811b89709cf43bfe6b9bb287484e81ab5277
Egeeoz <ozakdemirege@hotmail.com>
2023-11-30 14:37:16 +0100

    Added new html page for planet info, made planets clickable, onclick it will console.log information from api about that planet in the console

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let apiKey;
+let planetData;
+let clickedPlanet;
+
+let currentClickedPlanet = document.querySelector(".planets__container");
 
 async function getApiKey() {
   try {
-    const response = await fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    const response = await fetch(
+      "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     let data = await response.json();
     // Assign the obtained key to the global apiKey variable
@@ -20,22 +27,40 @@ async function getApiKey() {
 // Call the async function
 getApiKey().then(() => {
   // The apiKey is now updated, and you can use it here
-  console.log(apiKey);
 
-  async function getData(){
-    try{
-        const response = await fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies", {
-            method: "GET",
-            headers: {'x-zocom': apiKey }
-        })
+  async function getData() {
+    try {
+      const response = await fetch(
+        "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies",
+        {
+          method: "GET",
+          headers: { "x-zocom": apiKey },
+        }
+      );
 
-        let data = await response.json();
-        console.log(data.bodies);
-
-    } catch (error){
-        console.log(error)
+      let data = await response.json();
+      planetData = data.bodies;
+      console.log(planetData);
+    } catch (error) {
+      console.log(error);
     }
-}
+  }
 
-getData();
+  getData();
+});
+
+currentClickedPlanet.addEventListener("click", (event) => {
+  if (
+    event.target.tagName === "A" &&
+    event.target.classList.contains("clicked")
+  ) {
+    console.log(event.target.id);
+    clickedPlanet = event.target.id;
+
+    planetData.forEach((planetData) => {
+      if (clickedPlanet == planetData.name) {
+        console.log(planetData);
+      }
+    });
+  }
 });
